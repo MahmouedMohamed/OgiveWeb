211930acdd41466f4a45079425de50a6cee44e6c
mahmoued <mahmouedmartin222@yahoo.com>
2020-06-17 06:22:09 +0200

    Added oauth token refresher when login.

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -1,14 +1,12 @@
 <?php
 namespace App\Http\Controllers\api;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-
-use Response;
 
 class UserController extends Controller
 {
@@ -18,14 +16,19 @@ class UserController extends Controller
     public function login(){
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
-            $this->content['token'] =  $user->createToken($user->getAuthIdentifier())->accessToken;
+            $token = $user->accessTokens->where('revoked',0)->where('expires_at','>',Carbon::now());
+            if(!$token->isEmpty()){
+                $token[0]->delete();
+            }
+            $this->content['token'] =
+                    $user->createToken($user->getAuthIdentifier())->accessToken;
+
             $this->content['user'] = Auth::user();
             return response()->json($this->content);
         }
         else{
             $this->content['error'] = "Unauthorized";
-            $status = 401;
-            return response()->json($this->content, $status);
+            return response()->json($this->content);
         }
     }
 
