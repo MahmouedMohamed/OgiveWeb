571c958da08b96a39a340594eb25302c3cd91f69
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-01-07 16:58:42 +0200

    Modified user entity

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
+            $table->string('user_name')->unique();
             $table->string('email')->unique();
+            $table->string('gender')->default('female');
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
