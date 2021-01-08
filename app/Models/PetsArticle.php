933287b3c17cc7db2746584b3e54ca63483ab30c
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-01-08 14:48:58 +0200

    Added Pet Article model,factory,seeder,controller

---
+++ app/Models/PetsArticle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PetsArticle extends Model
+{
+    use HasFactory;
+}
