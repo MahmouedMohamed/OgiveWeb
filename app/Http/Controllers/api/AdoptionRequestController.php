633c534daf0814a8b446ce7607e6ec98af1bc3e2
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-01-16 20:15:51 +0200

    Added APIs Controllers Resources

---
+++ app/Http/Controllers/api/AdoptionRequestController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\api;
+use App\Http\Controllers\Controller;
+
+use App\Models\AdoptionRequest;
+use Illuminate\Http\Request;
+
+class AdoptionRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AdoptionRequest  $adoptionRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdoptionRequest $adoptionRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AdoptionRequest  $adoptionRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AdoptionRequest $adoptionRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AdoptionRequest  $adoptionRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdoptionRequest $adoptionRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AdoptionRequest  $adoptionRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdoptionRequest $adoptionRequest)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/api/ConsultationCommentController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\api;
+use App\Http\Controllers\Controller;
+
+use App\Models\ConsultationComment;
+use Illuminate\Http\Request;
+
+class ConsultationCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ConsultationComment  $consultationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ConsultationComment $consultationComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ConsultationComment  $consultationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ConsultationComment $consultationComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ConsultationComment  $consultationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ConsultationComment $consultationComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ConsultationComment  $consultationComment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ConsultationComment $consultationComment)
+    {
+        //
+    }
+}
