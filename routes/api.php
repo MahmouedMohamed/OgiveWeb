8618791a210f68f53f81ed387b2cbec475dee23b
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-01-16 20:20:00 +0200

    Added Pet Related APIs

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\api\UserController;
 use App\Http\Controllers\api\MarkersController;
 use App\Http\Controllers\api\MemoryController;
 use App\Http\Controllers\api\LikesController;
+use App\Http\Controllers\api\PetController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,14 +23,16 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('/marker',[MarkersController::class, 'getAll']);
     Route::post('/marker',[MarkersController::class, 'create']);
     Route::delete('/marker',[MarkersController::class, 'delete']);
-
     Route::get('/memory',[MemoryController::class, 'getAll']);
     Route::post('/memory',[MemoryController::class, 'create']);
     Route::delete('/memory',[MemoryController::class, 'delete']);
-
     Route::post('/like',[LikesController::class, 'likeUnlike']);
 });
-
-
+//ToDo: Add This APIs to be auth by 2oauth token
+Route::get('/pet',[PetController::class, 'index']);
+Route::get('/pet/{id}',[PetController::class, 'show']);
+Route::post('/pet',[PetController::class, 'store']);
+Route::patch('/pet/{id}',[PetController::class, 'update']);
+Route::delete('/pet/{id}',[PetController::class, 'destroy']);
 
 
