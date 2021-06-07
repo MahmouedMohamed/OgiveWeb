c0042a3d404a66c418cd3973337bdfc528a484e1
Mahmoued <Mahmouedmartin222@yahoo.com>
2021-06-07 07:17:22 +0200

    Added phone number to offline transaction

--- app/Models/OfflineTransaction.php
+++ app/Models/OfflineTransaction.php
@@ -14,6 +14,7 @@ class OfflineTransaction extends Model
         'amount',
         'preferredSection',
         'address',
+        'phoneNumber',
         'startCollectDate',
         'endCollectDate',
         'selectedDate',
