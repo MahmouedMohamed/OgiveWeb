fe926d3ebfcb0890c059ca30472ce555edb7cfc1
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-10-08 01:20:03 +0200

    Added User Functionality Validator With Exceptions

---
+++ app/Exceptions/UserNotAuthorized.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class UserNotAuthorized extends Exception
+{
+    /**
+     * Report the exception.
+     * @param \App\Models\User $user
+     *
+     * @param string $type
+     *
+     * @param $model
+     *
+     * @return bool|null
+     */
+    public function report(User $user, String $type, $model)
+    {
+        DB::table('exceptions_reports')->insert([
+            'user_id' => $user->id,
+            'type' => $type,
+            'details' => $user->name .
+                ' tried to access this ' .
+                class_basename($model) .
+                ' with id of ' . $model->id
+        ]);
+    }
+}
