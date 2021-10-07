6e9e60aa1a71bbd67ca3f143c68e01e67641cdcc
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-10-08 01:22:49 +0200

    Added Online Transaction Trait Functions with Exceptions

---
+++ app/Exceptions/OnlineTransactionNotFound.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class OnlineTransactionNotFound extends Exception
+{
+    /**
+     * Report the exception.
+     *
+     * @return bool|null
+     */
+    public function report()
+    {
+        //
+    }
+}
