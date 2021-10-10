b49d5de43a0a5dda00d67004801c5ed8ed70445b
Shrouk Sobhy <shroukksobhy@gmail.com>
2021-10-10 14:54:54 +0200

    Added Needy Factory

--- database/factories/NeedyFactory.php
+++ database/factories/NeedyFactory.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Database\Factories;
+
 use App\Models\User;
 
 use App\Models\Needy;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class NeedyFactory extends Factory
 {
@@ -22,20 +24,22 @@ class NeedyFactory extends Factory
      */
     public function definition()
     {
+        $type = ['إيجاد مسكن مناسب', 'تحسين مستوي المعيشة', 'تجهيز لفرحة', 'سداد الديون', 'إيجاد علاج'];
+
         return [
             //
             'name' => $this->faker->name(),
-            'type'=>'Dog',
-            'age'=>$this->faker->numberBetween(1,10),
-            'severity'=>$this->faker->numberBetween(1,10),
-            'details'=>$this->faker->text(500),
-            'collected'=>$this->faker->numberBetween(0,1),
-            'address'=>$this->faker->text(20),
-            'satisfied'=>$this->faker->numberBetween(0,1),
-            'approved'=>$this->faker->numberBetween(0,1),
-            "createdBy"=> User::inRandomOrder()->first(),
-            'created_at'=>$this->faker->dateTimeBetween('-4 week','now'),
-            'updated_at'=>$this->faker->dateTimeBetween('-4 week','now'),
+            "type" => Arr::random($type),
+            'age' => $this->faker->numberBetween(1, 10),
+            'severity' => $this->faker->numberBetween(1, 10),
+            'details' => $this->faker->text(500),
+            'collected' => $this->faker->numberBetween(0, 1),
+            'address' => $this->faker->text(20),
+            'satisfied' => $this->faker->numberBetween(0, 1),
+            'approved' => $this->faker->numberBetween(0, 1),
+            "createdBy" => User::inRandomOrder()->first(),
+            'created_at' => $this->faker->dateTimeBetween('-4 week', 'now'),
+            'updated_at' => $this->faker->dateTimeBetween('-4 week', 'now'),
         ];
     }
 }
