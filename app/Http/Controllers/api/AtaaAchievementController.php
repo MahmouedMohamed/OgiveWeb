aa8cb060a44d96df550568778f388ca4a8830206
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-10-12 06:30:57 +0200

    Complete Get Ataa Achievement Feature

--- app/Http/Controllers/api/AtaaAchievementController.php
+++ app/Http/Controllers/api/AtaaAchievementController.php
@@ -37,15 +37,31 @@ class AtaaAchievementController extends BaseController
             $markersCollected = $user->ataaAchievement->markers_collected ?? 0;
             $markersPosted = $user->ataaAchievement->markers_posted ?? 0;
 
+            //Latest can replace that?? Cause of events
             $ataaPrizesAcquiredByUser = DB::table('ataa_prizes')->leftJoin(
                 'user_ataa_acquired_prizes',
                 'ataa_prizes.id',
                 '=',
                 'user_ataa_acquired_prizes.prize_id'
-            )->where('user_id', $user->id)->get();
-            // return $ataaPrizesAcquiredByUser->sortBy('level',1,true)->first()->level;
-            return $ataaPrizesAcquiredByUser->pluck('level')->max() ?? 0;
-            // return $ataaPrizesAcquiredByUser;
+            )->where('user_id', $user->id)->get() ?? 0;
+            $highestPrizeAcquired = $ataaPrizesAcquiredByUser->sortBy('level', 1, true)->first()->level ?? 0;
+
+            $ataaBadgesAcquiredByUser = DB::table('ataa_badges')->leftJoin(
+                'user_ataa_acquired_badges',
+                'ataa_badges.id',
+                '=',
+                'user_ataa_acquired_badges.badge_id'
+            )->where('user_id', $user->id)->latest('user_ataa_acquired_badges.created_at')->first();
+            $latestBadgeAcquired = $ataaBadgesAcquiredByUser->name ?? null;
+
+            $response = [
+                'markers_collected' => $markersCollected,
+                'markers_posted' => $markersPosted,
+                'current_level' => $highestPrizeAcquired,
+                'latest_badge' => $latestBadgeAcquired
+            ];
+
+            return $this->sendResponse($response, 'User Achievement Returned Successfully');
         } catch (UserNotFound $e) {
             return $this->sendError($responseHandler->words['UserNotFound']);
         } catch (AtaaAchievementNotFound $e) {
