4167fded5d8541e76c50dc5a18b9ba6ac4f9f0d7
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2022-10-07 17:48:58 +0200

    Added Case Type Converter Model

---
+++ app/ConverterModels/CaseType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\ConverterModels;
+
+class CaseType
+{
+    public static $value = [
+        'Finding Better Place for Living' => 1,
+        'Upgrading Standard of Living' => 2,
+        'Preparing For Joy' => 3,
+        'Dept Paying' => 4,
+        'Finding a Cure' => 5,
+    ];
+
+    public static $text = [
+        1 => 'Finding Better Place for Living',
+        2 => 'Upgrading Standard of Living',
+        3 => 'Preparing For Joy',
+        4 => 'Dept Paying',
+        5 => 'Finding a Cure',
+    ];
+
+    public static $text_ar = [
+        1 => 'إيجاد مسكن مناسب',
+        2 => 'تحسين مستوي المعيشة',
+        3 => 'تجهيز لفرحة',
+        4 => 'سداد الديون',
+        5 => 'إيجاد علاج'
+    ];
+}
