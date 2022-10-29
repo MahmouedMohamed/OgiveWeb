ed55d60e40ce850c5ae838316601fa88e65bba72
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2022-10-29 03:28:44 +0200

    Updated Ataa Achievemet Policy with owner id

--- app/Models/Ataa/AtaaAchievement.php
+++ app/Models/Ataa/AtaaAchievement.php
@@ -18,7 +18,7 @@ class AtaaAchievement extends Model
     ];
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'owner_id');
     }
     public function incrementMarkersPosted()
     {
