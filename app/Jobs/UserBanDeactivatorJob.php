dff4b84d411fb88f8772e29431adba1fedd1a6a8
Mahmoued Mohamed <mahmoued.mohamed@dayra.co>
2023-06-14 01:29:59 +0300

    Move SQL Events to Jobs

---
+++ app/Jobs/UserBanDeactivatorJob.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\UserBan;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UserBanDeactivatorJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        return UserBan::where('end_at', '<=', Carbon::now())
+            ->where('active', '=', 1)
+            ->update(['active' => false]);
+    }
+}
