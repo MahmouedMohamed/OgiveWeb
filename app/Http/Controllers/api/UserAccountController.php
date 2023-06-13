3e17dec06a2c64d4bd6093adff6ab7d5c195f54c
Mahmoued Mohamed <mahmoued.mohamed@dayra.co>
2023-06-14 02:03:34 +0300

    Add User Account Functionalities

---
+++ app/Http/Controllers/api/UserAccountController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Requests\UserAccountDepositRequest;
+use App\Http\Requests\UserAccountWithdrawalRequest;
+use Exception;
+
+class UserAccountController extends BaseController
+{
+    public function deposit(UserAccountDepositRequest $request)
+    {
+        try {
+            //ToDo: Make Bank Account Transaction
+            $request->user->account()->increment('balance', $request['amount']);
+            return $this->sendResponse($request->user->account, __('General.UserAccountDepositSuccessMessage'));
+        } catch (Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
+
+    public function withdrawal(UserAccountWithdrawalRequest $request)
+    {
+        try {
+            $account = $request->user->account;
+            if($request['amount'] > $account->balance){
+                return $this->sendError(__('General.UserAccountWithdrawalNoBalanceErrorMessage'));
+            }
+            //ToDo: Make Bank Account Transaction
+            $account->decrement('balance', $request['amount']);
+            return $this->sendResponse($account, __('General.UserAccountDepositSuccessMessage'));
+        } catch (Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
+}
--- resources/lang/en/General.php
+++ resources/lang/en/General.php
@@ -8,4 +8,7 @@ return [
     'UserBanNotFound' => 'User Ban Cannot be Found',
     'InvalidAccess' => 'Invalid Access',
     'ValidationFailure' => 'Invalid Data',
+    'UserAccountDepositSuccessMessage' => 'The Amount Has been Added to your Account',
+    'UserAccountWithdrawalSuccessMessage' => 'The Amount Has been Withdrawn from your Account',
+    'UserAccountWithdrawalNoBalanceErrorMessage' => 'You Don\'t Have enough balance in your Account',
 ];
