319615479ef6450765d998e04b58f8fff797157c
Mahmoued Mohamed <mahmoued.mohamed@dayra.co>
2023-06-18 03:40:15 +0300

    Add entries to update in settings

--- app/Http/Controllers/api/UserSettingsController.php
+++ app/Http/Controllers/api/UserSettingsController.php
@@ -24,6 +24,7 @@ class UserSettingsController extends BaseController
             $settings = $settings->update([
                 'auto_donate' => $request['auto_donate'] ?? $settings->auto_donate,
                 'auto_donate_on_severity' => $request['auto_donate_on_severity'] ?? $settings->auto_donate_on_severity,
+                'allow_multiple_donation_for_same_needy' => $request['allow_multiple_donation_for_same_needy'] ?? $settings->allow_multiple_donation_for_same_needy,
                 'min_amount_per_needy_for_auto_donation' => $request['min_amount_per_needy_for_auto_donation'] ?? $settings->min_amount_per_needy_for_auto_donation,
                 'max_amount_per_needy_for_auto_donation' => $request['max_amount_per_needy_for_auto_donation'] ?? $settings->max_amount_per_needy_for_auto_donation,
             ]);
